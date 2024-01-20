16cce267b824817e8179ce2cafd9f2867a72ec79
Shakib <its.shakib@proton.me>
2024-01-20 10:59:38 +0600

    rock: Lineage-21
    Signed-off-by: Shakib <its.shakib@proton.me>

--- lineage_rock.mk
+++ lineage_rock.mk
@@ -11,31 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/rock/device.mk)
 
-# Inherit some common AfterLifeProject stuff
-$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+# Inherit some common lineageProject stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Bootanimation Res
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# AfterLife flags
-AFTERLIFE_MAINTAINER := Shakib (@Shakib_BD)
-
-# Gapps/Vanilla Flags
-AFTERLIFE_GAPPS=true
-
-# Bootanimation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Offline Charging
-USE_PIXEL_CHARGING := true
-
-# disable/enable blur support, default is false
-TARGET_SUPPORTS_BLUR := false
-
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device Platform
 PRODUCT_DEVICE := rock
-PRODUCT_NAME := afterlife_rock
+PRODUCT_NAME := lineage_rock
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22071219AI
 PRODUCT_MANUFACTURER := xiaomi
