78ad7cc52de3a09da39334d3cf2fc8581ca0f66e
Shakib <its.shakib@proton.me>
2024-03-10 12:21:59 +0600

    rock: LineageOS-14
    Signed-off-by: Shakib <its.shakib@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2019 Potato Open Sauce Project
-# Copyright (C) 2020 The afterlifeOS Project
+# Copyright (C) 2020 The lineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,9 +16,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/afterlife_rock.mk
+    $(LOCAL_DIR)/lineage_rock.mk
 
 COMMON_LUNCH_CHOICES := \
-    afterlife_rock-user \
-    afterlife_rock-userdebug \
-    afterlife_rock-eng
+    lineage_rock-user \
+    lineage_rock-userdebug \
+    lineage_rock-eng
