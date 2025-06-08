b1895e39ce493eb0f70398f9a242bcdab0bb74bf
Shakib <dev@shakib.my.id>
2025-06-08 06:41:09 +0600

    rock: vendorsetup: Add `libcrypto` and `libssl v32` to fetch halcyon-project's hardware/lineage/compat
    Signed-off-by: Shakib <dev@shakib.my.id>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,6 @@
 git clone -b 15-QPR2 https://github.com/mondrian-x/packages_apps_JamesDSPManager.git packages/apps/JamesDSPManager
+#---------- Fuck libssl-v32.so & libcrypto-v32.so --------#
+cd hardware/lineage/compat
+git fetch https://github.com/halcyonproject/hardware_halcyon_compat.git
+git cherry-pick 1cdc460268c0f967144da4b69eabbabbffec7195
+cd -
